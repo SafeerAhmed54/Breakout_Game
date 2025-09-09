4ae3454ef1aac743a203a271e9518c5d27f6491b
Safeer Ahmad Rana <pakistanireacts9@gmail.com>
2025-09-09 23:57:44 +0500

    Update Bricks.cs

--- Assets/Scripts/Bricks/Bricks.cs
+++ Assets/Scripts/Bricks/Bricks.cs
@@ -2,15 +2,12 @@ using UnityEngine;
 
 public class Bricks : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (collision.CompareTag("Ball"))
+        {
+            //Destroy(gameObject);
+            gameObject.SetActive(false);
+        }
     }
 }
