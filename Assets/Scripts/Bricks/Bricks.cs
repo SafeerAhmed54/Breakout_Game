dc9cd612bd16f7c7c2e7d8e85287a8fa9c33e78f
Safeer Ahmad Rana <pakistanireacts9@gmail.com>
2025-09-11 23:53:58 +0500

    Updated Brick Script

--- Assets/Scripts/Bricks/Bricks.cs
+++ Assets/Scripts/Bricks/Bricks.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class Bricks : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.CompareTag("Ball"))
+        if (collision.gameObject.CompareTag("Ball"))
         {
             //Destroy(gameObject);
             gameObject.SetActive(false);
