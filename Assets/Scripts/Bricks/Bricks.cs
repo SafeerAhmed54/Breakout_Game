655d7d918018294270b9fd2ba8c2ae3e5a78b621
Safeer Ahmad Rana <pakistanireacts9@gmail.com>
2025-09-17 14:35:23 +0500

    Update Bricks.cs

--- Assets/Scripts/Bricks/Bricks.cs
+++ Assets/Scripts/Bricks/Bricks.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Bricks : MonoBehaviour
 {
+    // Collision Detection for Ball
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ball"))
