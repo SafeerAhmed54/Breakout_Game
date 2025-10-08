b90f106a0d476560b0afbdfd1cbb851636e7ff5c
Safeer Ahmad Rana <pakistanireacts9@gmail.com>
2025-10-08 23:29:39 +0500

    Updated UIManager

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private TextMeshProUGUI levelNum;
+    [SerializeField] private TextMeshProUGUI scoreNum;
     void Start()
     {
-        
+        levelNum.text = "Level " + 1;
+        scoreNum.text = "Score " + 1;
     }
 
     // Update is called once per frame
