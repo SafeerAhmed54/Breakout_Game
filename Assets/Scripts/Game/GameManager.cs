9364ad738c84e8e9f3e4effba3787ec600d1d86b
Safeer Ahmad Rana <pakistanireacts9@gmail.com>
2025-10-25 23:52:53 +0500

    Update GameManager.cs

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private int score;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        score = 0;
     }
 
     // Update is called once per frame
